94c7e42bbde0f9af1ad6fc7c022a67c2b910b5e0
Mic-9 <116760683+Mic-9@users.noreply.github.com>
2024-07-09 23:46:08 +0200

    inizio set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import linkedin from "./img/linkedin-icon.svg";
+import github from "./img/logo-github.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ricette Vegetariane</h1>
+      <footer>
+        <div className="f-container">
+          <div className="f-item">Michele Maioli &copy; 2024</div>
+          <div className="f-item">
+            <a id="git" href="https://github.com/Mic-9" target="_blank">
+              <img src={github} alt="GitHub Logo" />
+            </a>
+            <a
+              id="link"
+              href="https://www.linkedin.com/in/michelemaioli"
+              target="_blank"
+            >
+              <img src={linkedin} alt="LinkedIn Icon" />
+            </a>
+          </div>
+        </div>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
