0778e66fe1f21141a7c353c20516ccb7dc2fbf17
Mic-9 <116760683+Mic-9@users.noreply.github.com>
2024-07-11 23:16:08 +0200

    primo componente

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import React from "react";
-import linkedin from "./img/linkedin-icon.svg";
-import github from "./img/logo-github.svg";
-import "./App.css";
+import Footer from "./Footer";
+import "./css/App.css";
 
 const App = () => {
   return (
     <div className="App">
       <h1>Ricette Vegetariane</h1>
-      <footer id="footer">
-        <div className="f-container">
-          <div className="f-item">Michele Maioli &copy; 2024</div>
-          <div className="f-item">
-            <a id="git" href="https://github.com/Mic-9" target="_blank">
-              <img src={github} alt="GitHub Logo" />
-            </a>
-            <a
-              id="link"
-              href="https://www.linkedin.com/in/michelemaioli"
-              target="_blank"
-            >
-              <img src={linkedin} alt="LinkedIn Icon" />
-            </a>
-          </div>
-        </div>
-      </footer>
+      <Footer></Footer>
     </div>
   );
 };
---
+++ src/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import linkedin from "./img/linkedin-icon.svg";
+import github from "./img/logo-github.svg";
+import "./css/Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="Footer" id="footer">
+      <div className="F-container">
+        <div className="F-item">Michele Maioli &copy; 2024</div>
+        <div className="F-item">
+          <a id="git" href="https://github.com/Mic-9" target="_blank">
+            <img src={github} alt="GitHub Logo" />
+          </a>
+          <a
+            id="link"
+            href="https://www.linkedin.com/in/michelemaioli"
+            target="_blank"
+          >
+            <img src={linkedin} alt="LinkedIn Icon" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
